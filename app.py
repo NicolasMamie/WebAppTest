3e665b69d7e00a72a13cb119654b24f2cf809c18
NicolasMamie <n.mamie@soltar.biz>
2025-06-19 10:16:17 +0200

    test

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '''
+    <h2>Upload Excel File</h2>
+    <form method="POST" action="/upload" enctype="multipart/form-data">
+      <input type="file" name="file">
+      <input type="submit">
+    </form>
+    '''
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+    # You can process the file here
+    return f"Received file: {file.filename}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
