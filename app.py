a7e49ce4165e46f4e5a492bae1b8dec4a2cefeb4
NicolasMamie <n.mamie@soltar.biz>
2025-06-19 16:36:05 +0200

    added bars

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, request
 import os
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
+import datetime as dt
 
 app = Flask(__name__)
 
@@ -14,24 +16,98 @@ def index():
         file = request.files['file']
         df = pd.read_excel(file)
 
+        
         # Table preview
         preview_html = df.head().to_html(classes='table table-striped', index=False)
 
         # Plot first numeric column
         numeric_cols = df.select_dtypes(include='number').columns
+        date_cols = df.select_dtypes(include=['datetime', 'datetime64']).columns
         if not numeric_cols.any():
             chart_html = "<p>No numeric columns to plot.</p>"
         else:
-            x_col = numeric_cols[0]
-            y_col = numeric_cols[1]
-            fig = px.line(df, x = x_col, y=y_col, title=f"Line Chart of '{y_col}'")
-            chart_html = fig.to_html(full_html=False)
+            date_col_in = date_cols[0]
+            date_col_out = date_cols[1]
+            
+            
+           
+            col_in = numeric_cols[0]
+            col_out = numeric_cols[1]
+
+            df_in = df[[date_col_in, col_in]]
+            df_in['Type'] = 'Purchase'
+            df_in.rename(columns={'Time_in':'Time', 'In': 'Quantity'}, inplace=True)
+
+            df_out = df[[date_col_out, col_out]]
+            df_out['Type'] = 'Sale'
+            df_out.rename(columns={'Time_out':'Time', 'Out':'Quantity'}, inplace=True)
+
+            if df_out['Quantity'].sum() > 0:
+                df_out['Quantity'] = df_out['Quantity']*(-1)
+
+            df_combined = pd.concat([df_in, df_out])
+
+           
+            df_combined['Time'] = pd.to_datetime(df_combined['Time']).dt.date
+            df_combined = df_combined.sort_values(by = 'Time' )
+            df_combined.dropna(inplace=True)
+
+            print(df_combined)
+          
+         
+            fig = go.Figure()
+
+            '''
+           
+            fig.add_trace(go.Bar(
+                    x=df_combined['Time'],
+                    y=df_combined['Quantity'],
+                    name=col_in,
+                    marker_color='green',
+                    width=0.05
+                ))
+            '''
+            df_combined['Time'] = pd.to_datetime(df_combined['Time'])
+            # Purchases (green)
+            df_purchases = df_combined[df_combined['Type'] == 'Purchase']
+            fig.add_trace(go.Bar(
+                x=df_purchases['Time'],
+                y=df_purchases['Quantity'],
+                name='Purchase',
+                marker_color='green',
+                width=0.5
+            ))
+
+            # Sales (red)
+            df_sales = df_combined[df_combined['Type'] == 'Sale']
+            fig.add_trace(go.Bar(
+                x=df_sales['Time'],
+                y=df_sales['Quantity'],
+                name='Sale',
+                marker_color='red',
+                width=0.5
+            ))
+            
+            fig.update_layout(
+                title=f"Bar Chart of '{col_in}'",
+                xaxis_title='Time',
+                yaxis_title='QTY',
+                #xaxis=dict(type='category'),  # Use 'category' if x_col is datetime and not continuous
+                xaxis=dict(
+                    type='date',
+                    tickformat='%Y-%m-%d'
+                )
+                
+            )
+            chart_html = fig.to_html(full_html=True)
+
 
     return f'''
     <!DOCTYPE html>
     <html>
     <head>
         <title>Excel Uploader</title>
+        <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
         <style>
             .section-title {{
@@ -44,6 +120,9 @@ def index():
                 background-color: #ccc;
                 margin-bottom: 1rem;
                 max-width: 600px;
+            }}
+            table {{
+                text-align: right;
             }}
         </style>
     </head>
